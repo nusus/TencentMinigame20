744d44161bb08c856d3560bee8b3ba1c9bb8bc11
franciszeng <franciszeng@FRANCISZENG-PC0>
2016-09-05 10:44:31 +0800

    添加TexturePacker、Unity Remote

---
+++ Assets/Scripts/GyroscopeInfo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GyroscopeInfo : MonoBehaviour {
+
+    private Vector3 m_RotateAngle;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        int fingerCount = 0;
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+                fingerCount++;
+
+        }
+        if (fingerCount > 0)
+            print(string.Format("x:{0}, y:{1}, z:{2}", Input.acceleration.x, Input.acceleration.y, Input.acceleration.z));
+    }
+
+}
