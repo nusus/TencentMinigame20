4c6b489318ae1b2b763e2809b7a788cf6eac34a5
franciszeng <franciszeng@FRANCISZENG-PC0>
2016-09-06 20:15:40 +0800

    添加重力感应

--- Assets/Scripts/BabyController.cs
+++ Assets/Scripts/BabyController.cs
@@ -2,14 +2,18 @@
 using System.Collections;
 
 public class BabyController : MonoBehaviour {
+    public GyroscopeInfo m_GyroscopeInfo;
 
+    private Vector3 m_OrignalPosition;
 	// Use this for initialization
 	void Start () {
+        m_OrignalPosition = this.gameObject.transform.position;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        this.gameObject.transform.position = m_GyroscopeInfo.RotateCoordinate(m_OrignalPosition);
+     
 	}
 }
--- Assets/Scripts/GyroscopeInfo.cs
+++ Assets/Scripts/GyroscopeInfo.cs
@@ -3,15 +3,30 @@ using System.Collections;
 
 public class GyroscopeInfo : MonoBehaviour {
 
-    private Vector3 m_RotateAngle;
+    private float m_CurrentAngles;
+    private float m_LastTargetAngles;
+
+    public float m_AngularVelocity;
+    public float m_DenoiseRange;
 	// Use this for initialization
 	void Start () {
-	
-	}
+        m_CurrentAngles = 0.0f;
+        m_LastTargetAngles = 0.0f;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.acceleration.x >=0)
+        {
+
+        }
+
+        float targetAngles = Mathf.Asin(-Input.acceleration.x);
+        targetAngles = Denoise(m_LastTargetAngles, targetAngles, m_DenoiseRange);
+        m_LastTargetAngles = targetAngles;
+        m_CurrentAngles = AngularInterpolation(m_CurrentAngles, targetAngles, m_AngularVelocity);
+
         int fingerCount = 0;
         foreach (Touch touch in Input.touches)
         {
@@ -19,21 +34,53 @@ public class GyroscopeInfo : MonoBehaviour {
                 fingerCount++;
 
         }
+
         if (fingerCount > 0)
-            print(string.Format("x:{0}, y:{1}, z:{2}", Input.acceleration.x, Input.acceleration.y, Input.acceleration.z));
+            print(targetAngles);
+
+    }
+
+    private float AngularInterpolation(float currentAngles, float targetAngles, float angleVelocity)
+    {
+        if (currentAngles + angleVelocity >= targetAngles)
+        {
+            return targetAngles;
+        }
+        return currentAngles + angleVelocity;
+    }
+
+    private float Denoise(float prev, float curr, float rang)
+    {
+        if (Mathf.Abs(curr - prev) <= rang)
+        {
+            return prev;
+        }
+        return curr;
     }
 
 
     public float GetSinRotateAngle()
     {
-        return -Input.acceleration.x;
+        return Mathf.Sin(m_CurrentAngles);
+        //return -Input.acceleration.x;
+        //return Mathf.Sin(m_RotateAngles);
     }
 
     public float GetCosRotateAngle()
     {
-        return -Input.acceleration.y;
+        return Mathf.Cos(m_CurrentAngles);
+        //return -Input.acceleration.y;
+        //return Mathf.Cos(m_RotateAngles);
     }
 
-    public 
+    public Vector3 RotateCoordinate(Vector3 origVec3)
+    {
+        Vector2 row1 = new Vector2(GetCosRotateAngle(), GetSinRotateAngle());
+        Vector2 row2 = new Vector2(-GetSinRotateAngle(), GetCosRotateAngle());
+        Vector2 coor = new Vector2(origVec3.x, origVec3.y);
+        origVec3.x = Vector2.Dot(row1, coor);
+        origVec3.y = Vector2.Dot(row2, coor);
+        return origVec3;
+    }
 
 }
