c8849d7c839a06a7894f62a7afed2bbcd73133ba
sisiwu <sisiwu@SISIWU-NB0>
2016-09-11 20:38:30 +0800

    sisiwu

---
+++ Assets/Scripts/menuScript.cs
@@ -0,0 +1,32 @@
+using UnityEngine;  
+using System.Collections;  
+using UnityEngine.UI;  
+  
+  
+public class menuScript : MonoBehaviour {  
+    public Canvas Canvasiswater;
+    public Button Buttonfood;
+	public Button Buttonwater;
+	public GameObject mainCamera;
+	
+    // Use this for initialization  
+    void Start () {
+        Canvasiswater.enabled = false;   
+    }  
+  
+    public void waterClick()  
+    {
+        Canvasiswater.enabled = true;
+    }  
+      
+    //yes°´Å¥
+    public void yeswaterPress()  
+    {
+        Canvasiswater.enabled = false;
+    }
+
+    public void nowaterPress()  
+    {
+        Canvasiswater.enabled = false;
+    }
+}  
