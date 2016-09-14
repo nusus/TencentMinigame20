e46ee8d0d218cb09c162da1ee7dbfa6fdf8f407c
sisiwu <sisiwu@SISIWU-NB0>
2016-09-14 14:44:56 +0800

    ss

---
+++ Assets/watertime.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class watertime : MonoBehaviour {
+
+	public Transform weishuiPanel;
+	public Transform weishuijieguoPanel;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("LaunchweishuijieguoPanel", 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LaunchweishuijieguoPanel(){
+		weishuijieguoPanel.gameObject.SetActive(true);
+	}
+}
