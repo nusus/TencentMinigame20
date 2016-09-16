25905b560192a1553188869b1c0d84e441861331
sisiwu <sisiwu@SISIWU-NB0>
2016-09-16 16:52:14 +0800

    ss

--- Assets/weishuijieguoPanelCtrl.cs
+++ Assets/weishuijieguoPanelCtrl.cs
@@ -7,10 +7,13 @@ public class weishuijieguoPanelCtrl : MonoBehaviour {
     public GameObject weishuiPanel = null;
     public Button yesButton = null;
     public Button noButton = null;
+	public Button closeButton = null;
 
 	// Use this for initialization
 	void Start () {
         EventTriggerListener.Get(yesButton.gameObject).onClick = OnYesBtnClick;
+		EventTriggerListener.Get(noButton.gameObject).onClick = OnNoBtnClick;
+		EventTriggerListener.Get(closeButton.gameObject).onClick = OnNoBtnClick;
 	}
 	
 	// Update is called once per frame
@@ -31,6 +34,11 @@ public class weishuijieguoPanelCtrl : MonoBehaviour {
 
     private void OnNoBtnClick(GameObject go)
     {
-
+		this.gameObject.SetActive(false);
+		weishuiPanelCtrl weishuiCtrl = weishuiPanel.GetComponent<weishuiPanelCtrl>();
+		if (weishuiCtrl != null)
+		{
+			weishuiCtrl.StopTick();
+		}
     }
 }
