53321f116c70b1a0d30197823d50c1575b07c0ff
sisiwu <sisiwu@SISIWU-NB0>
2016-10-05 17:04:31 +0800

    主界面UI图更换

---
+++ Assets/ChangeLV.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLV : MonoBehaviour {
+	public UnityEngine.UI.Image m_LVTenNumImage;
+	public UnityEngine.UI.Image m_LVUnitNumImage;
+	private int m_LVNum;
+
+	public Sprite[] m_Images;
+	// Use this for initialization
+	void Start () {
+		m_LVNum = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp (0)) {
+			m_LVNum += 1;
+			ChangeNumberSprite (m_LVNum);
+		}
+	}
+
+	public void ChangeNumberSprite(int number){
+		int ten = number / 10;
+		int unit = number - ten * 10;
+		m_LVTenNumImage.sprite = m_Images [ten];
+		m_LVUnitNumImage.sprite = m_Images [unit];
+	}
+}
---
+++ Assets/ChangeNumber.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeNumber : MonoBehaviour {
+	public UnityEngine.UI.Image m_HealthTenNumberImage;
+	public UnityEngine.UI.Image m_HealthUnitNumberImage;
+	private int m_HealthNumber;
+
+	public UnityEngine.UI.Image m_RMBhundNumImage;
+	public UnityEngine.UI.Image m_RMBTenNumImage;
+	public UnityEngine.UI.Image m_RMBUnitNumImage;
+	private int m_RMBNum;
+
+	public Sprite[] m_Images;
+	//private Sprite m_GoldNumber
+	// Use this for initialization
+
+	void Start () {
+		m_HealthNumber = 1;
+		m_RMBNum = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp (0)) {
+			m_HealthNumber += 1;
+			m_RMBNum += 10;
+			ChangehealNumberSprite (m_HealthNumber,m_HealthTenNumberImage,m_HealthUnitNumberImage);
+			ChangeRMBNumberSprite (m_RMBNum,m_RMBhundNumImage,m_RMBTenNumImage,m_RMBUnitNumImage);
+		}
+	
+	}
+
+	public void ChangehealNumberSprite(int number,UnityEngine.UI.Image b,UnityEngine.UI.Image c){
+		int ten = number / 10;
+		int unit = number - ten * 10;
+		b.sprite = m_Images [ten];
+		c.sprite = m_Images [unit];
+	}
+
+	public void ChangeRMBNumberSprite(int number,UnityEngine.UI.Image a,UnityEngine.UI.Image b,UnityEngine.UI.Image c){
+		int hund = number / 100;
+		int ten = (number - hund*100 )/ 10;
+		int unit = number - hund*100 - ten * 10;
+		a.sprite = m_Images [hund];
+		b.sprite = m_Images [ten];
+		c.sprite = m_Images [unit];
+	}
+}
