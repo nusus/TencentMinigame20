d4235180a68a70ebc42714fcfcda56982a3bb0ad
gotoli(李立) <gotoli@tencent.com>
2016-10-06 16:37:20 +0800

    merge ui 2

---
+++ Assets/Food2Panel.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class Food2Panel : MonoBehaviour {
+    public GameObject foodPanel = null;
+    public Button yesfoodButton = null;
+    public Button nofoodButton = null;
+    public Button closeButton = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        EventTriggerListener.Get(yesfoodButton.gameObject).onClick = OnYesBtnClick;
+        EventTriggerListener.Get(nofoodButton.gameObject).onClick = OnNoBtnClick;
+        EventTriggerListener.Get(closeButton.gameObject).onClick = OnNoBtnClick;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnYesBtnClick(GameObject go)
+    {
+        this.gameObject.SetActive(true);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("FeedFood");
+    }
+
+    private void OnNoBtnClick(GameObject go)
+    {
+        /*this.gameObject.SetActive(false);
+        weishuiPanelCtrl weishuiCtrl = weishuiPanel.GetComponent<weishuiPanelCtrl>();
+        if (weishuiCtrl != null)
+        {
+            weishuiCtrl.StopTick();
+        }
+        */
+    }
+}
