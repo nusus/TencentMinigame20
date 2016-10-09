692fd25ffb9710c484d7a109efbbf082e5ec46f2
Coresi7 <794347466@qq.com>
2016-10-09 22:05:14 +0800

    actions

--- Assets/Scripts/MainScene/UI/MainUIBaby.cs
+++ Assets/Scripts/MainScene/UI/MainUIBaby.cs
@@ -16,19 +16,26 @@ public class MainUIBaby : MonoBehaviour {
     public void OnThirstLowerStandard()
     {
         GetComponent<Animator>().SetBool("isThirsty", true);
-        GetComponent<RandomAction>().isIdle = 0;
+        //GetComponent<RandomAction>().isIdle = 0;
     }
 
     public void OnHungerLowerStandard()
     {
         GetComponent<Animator>().SetBool("isHungry", true);
-        GetComponent<RandomAction>().isIdle = 0;
+        //GetComponent<RandomAction>().isIdle = 0;
     }
 
     public void OnEnergyLowerStandard()
     {
         GetComponent<Animator>().SetBool("isLowEnergy", true);
-        GetComponent<RandomAction>().isIdle = 0;
+        //GetComponent<RandomAction>().isIdle = 0;
     }
-
+    public void ExitException()
+    {
+        //exit unusual state.
+        //GetComponent<RandomAction>().isIdle = 1;
+        GetComponent<Animator>().SetBool("isThirsty", false);
+        GetComponent<Animator>().SetBool("isHungry", false);
+        GetComponent<Animator>().SetBool("isLowEnergy", false);
+    }
 }
